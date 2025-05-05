a44bb6f465978ab01aa1279b48312df8635ad6ac
John Khoo <johnkhootf@gmail.com>
2025-05-05 13:13:03 +0800

    gc sockets closed by remote end

--- src/loop.cpp
+++ src/loop.cpp
@@ -11,8 +11,8 @@ void SocketGC::gc() {
     // may decrease latency but not sure if will lead to fd starvation at scale
     while (!q.empty()) {
       int fd = q.front();
-      int rem;
-      if (ioctl(fd, TIOCOUTQ, &rem) < 0) {
+      int rem, n;
+      if (ioctl(fd, TIOCOUTQ, &rem) < 0 || ioctl(fd, FIONREAD, &n) < 0) {
         pp::log_error("Error when checking socket data: {}\n", strerror(errno));
         close(fd);
         q.pop();
@@ -21,6 +21,11 @@ void SocketGC::gc() {
 
       if (rem > 0) {
         pp::debug("gc: {} bytes left to send, not closing yet\n", rem);
+        break;
+      }
+      if (n == 0) {
+        pp::debug("gc: socket closed by remote peer", rem);
+        close(fd);
         break;
       }
       pp::debug("gc: closing socket\n");
